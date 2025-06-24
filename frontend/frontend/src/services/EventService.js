4979532fbdb98d29db77fc816ee99ff349ac634c
anajaneska <lindalea202@gmail.com>
2025-06-24 15:20:33 +0200

    changes

--- frontend/frontend/src/services/EventService.js
+++ frontend/frontend/src/services/EventService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/events";
+const API_URL = "http://localhost:8080/api/events";
 
 export const getEvents = () => axios.get(API_URL);
 export const getEventById = (id) => axios.get(`${API_URL}/${id}`);
